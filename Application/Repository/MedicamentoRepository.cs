149faa1dba77931f6393cac200a2c84fcc8b1a43
Nicolas sarchi <nicolassarchipico@gmail.com>
2023-09-20 00:44:57 -0500

    feat: :sparkles: Metodos para medicamento

--- Application/Repository/MedicamentoRepository.cs
+++ Application/Repository/MedicamentoRepository.cs
@@ -8,15 +8,27 @@ namespace Application.Repository
 {
     public class MedicamentoRepository : GenericRepository<Medicamento>, IMedicamento
     {
-     private readonly FarmaciaDBContext _context;
+        private readonly FarmaciaDBContext _context;
         public MedicamentoRepository(FarmaciaDBContext context) : base(context)
         {
             _context = context;
         }
 
-   public override async Task<IEnumerable<Medicamento>> GetAllAsync()
-{
- return await _context.Medicamentos.ToListAsync();
-}  
-}
+        public override async Task<IEnumerable<Medicamento>> GetAllAsync()
+        {
+            return await _context.Medicamentos.ToListAsync();
+        }
+        public async Task<IEnumerable<Medicamento>> GetMenos50UnidadesAsync()
+        {
+            return await _context.Medicamentos
+                .Where(m => m.Stock < 50)
+                .ToListAsync();
+        }
+        public async Task<Medicamento> ObtenerMedicamentoMasCaroAsync()
+        {
+            return await _context.Medicamentos
+                .OrderByDescending(m => m.PrecioMedicamento)
+                .FirstOrDefaultAsync();
+        }
+    }
 }
