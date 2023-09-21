57090f1799d008a742f0a742222ec3c0feb2fae2
Nicolas Sarchi <nicolassarchipico@gmail.com>
2023-09-21 14:55:33 -0500

    feat: :sparkles: Consultas de venta

--- Application/Repository/MedicamentoVentaRepository.cs
+++ Application/Repository/MedicamentoVentaRepository.cs
@@ -44,6 +44,18 @@ public class MedicamentoVentaRepository : GenericRepository<MedicamentoVenta>, I
 
         return pacientesQueCompraronParacetamol;
     }
+
+    public async Task<IEnumerable<FacturaBase>> GetMedicamentosVendidosMarzoAsync()
+    {
+        DateTime inicioMarzo = new DateTime(2023, 03, 01);
+        DateTime FinalMarzo = new DateTime(2023, 03, 31);
+
+        var medicamentosVendidosMarzo = await _context.FacturaBases
+        .Where(fb => fb.FechaSalida > inicioMarzo && fb.FechaSalida < FinalMarzo)
+        .ToListAsync();
+
+        return medicamentosVendidosMarzo;
+    }
 }
 
 
